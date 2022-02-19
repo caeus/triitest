53a1a6204545b9336e94f506137eb347385cacf6
caeus <camilo.a.navas@gmail.com>
2022-02-19 13:25:36 -0500

    It took me way more than one hour

--- stocks.py
+++ stocks.py
@@ -26,19 +26,69 @@ def get_price_safe(stock:Stock):
     try:
         return get_stock_data(stock)['price']
     except:
-        return False
+        raise Exception(f'No stock price for {stock}')
 
 
+
+## for dynamic programming
+example_item = {
+    "value": 100, ## The higher, the better
+    "diversification":{'AAPL':1,'GOOGL':0} ## ETC
+}
+## every item here should be of the previous structure
+cache = {}
+
+def optimize_aux(prices, i, amount, cache):
+    '''
+        Basically I used the 0/1 knapsack problem with a tweak to transform it 
+        into a unbounded knapsack problem
+    '''
+    key = f'{i}:{amount}' 
+    ##print(key)
+    if key in cache:
+        return cache[key]
+    if i == -1 or amount == 0:
+        result =  {'value':0, 'diversification':{}}
+    elif prices[i]['price'] > amount:
+        result = optimize_aux(prices, i-1, amount, cache)
+    else:
+        dont_take_it = optimize_aux(prices, i-1, amount,cache)
+        '''
+        As you can see, here I don't subtract from i, allowing the algorithm to choose the same stock 
+        more than once
+        '''
+        take_it = optimize_aux(prices, i, amount-prices[i]['price'], cache)
+        if(take_it['value']+ prices[i]['price'] > dont_take_it['value']):
+            diversification = take_it['diversification'].copy()
+            diversification[prices[i]['stock']] = diversification.get(prices[i]['stock'],0)+1
+            result = {
+                'value': take_it['value']+ prices[i]['price'],
+                'diversification': diversification
+            }
+        else:
+            result = dont_take_it
+    cache[key] = result
+    return result
 
 def optimize(amount:float):
-    prices= {stock.name:get_price_safe(stock) for stock in list(Stock)}
-    prices = {k:v for k,v in prices.items() if v!=False}
-    min_price = min([v for k,v in prices.items()])
-    nrmlzd_prices = {k:(v/min_price) for k,v in prices.items()}
-    capacity = amount/min_price
-    if(capacity<1):
-        raise Exception("Not enough money")
-    print(nrmlzd_prices)
+    prices= [{'stock':stock.name,'price':get_price_safe(stock)} for stock in list(Stock)]
+    prices = [price for price in prices if price['price']!=False]
+    min_amount_allowed = sum([price['price'] for price in prices])
+    '''
+    I do this, given the fact that I have to choose all stock at least once,
+    so I buy one of every, and then do the optimization with the rest of the money
+    '''
+    if(amount<min_amount_allowed):
+        raise Exception(f"Not enough money, it needs to be greated or equal than {min_amount_allowed}")
+    
+    result= optimize_aux(prices,len(prices)-1,amount-min_amount_allowed,{})
+    
+    return {
+        'remaining':amount-result['value'],
+        'diversification':{ 
+            price['stock']:result['diversification'].get(price['stock'],0)+1 for price in prices
+            }}
+
 
 
 ## For price of acquiring it all
@@ -59,6 +109,6 @@ args= parser.parse_args()
 
 
 if(args.option == 'optimize'):
-    optimize(args.amount)
+    print(optimize(args.amount))
 elif(args.option == 'all'):
     print(all(args.stock))
